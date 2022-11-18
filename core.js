51cec767bda9a364031de2cda56a6cf52784b1d1
evayann <yann.zavattero@hotmail.com>
2022-11-18 11:48:42 +0100

    V1.2

--- core.js
+++ core.js
@@ -2,26 +2,46 @@ const gba = document.getElementById("gba");
 const screen = document.getElementById("screen");
 const battle = document.getElementById("battle");
 
+const hide = "hidden";
+
 let previousScreen = 0;
 // Loading
 
 function loadMap(id) {
     console.log("Load Map :", id);
     for (let map of screen.children) {
-        if (! map.classList.contains("hidden"))
+        if (! map.classList.contains(hide))
             previousScreen = +map.id;
-        map.classList.toggle("hidden", +map.id !== id);
+        map.classList.toggle(hide, +map.id !== id);
     }
 }
 
 function loadBattle() {
     console.log("Load Battle");
+    
+    const [myPkm, opponentPkm] = loadPokemon();
+    loadPkmImg(false, myPkm.name);
+    loadPkmImg(true, opponentPkm.name);
+    loadAttacks(myPkm.attacks);
+
     loadMap(-1);
-    battle.classList.remove("hidden");
+    battle.classList.remove(hide);
+}
+
+function loadPkmImg(isOpponent, pkmName) {
+    const imgPkm = document.getElementById(isOpponent ? "opponent-pkm" : "my-pkm");
+    imgPkm.src = `./assets/pokemons/${pkmName}${isOpponent ? 'Front' : 'Back'}.png`;
+}
+
+function loadAttacks(attacks) {
+    for (let [i, attack] of attacks.entries()) {
+        const attackArea = document.getElementById(`attack-${i + 1}`);
+        attackArea.innerHTML = attack;
+    }
 }
 
 function closeBattle() {
-    battle.classList.add("hidden");
+    battle.classList.add(hide);
     const lastPrevious = previousScreen;
     loadMap(lastPrevious);
     previousScreen = lastPrevious;
@@ -32,12 +52,12 @@ function openDialog(content) {
     dialogText.innerHTML = content;
 
     const dialog = document.getElementById("dialog");
-    dialog.classList.remove("hidden");
+    dialog.classList.remove(hide);
 }
 
 function closeDialog() {
     const dialog = document.getElementById("dialog");
-    dialog.classList.add("hidden");
+    dialog.classList.add(hide);
 }
 
 // Screen Dimension 
@@ -54,3 +74,41 @@ function responsiveScreen() {
 
 window.onresize = responsiveScreen;
 responsiveScreen(); // Init default size
+
+// Pokemon
+
+class Pokemon {
+    constructor(name, attacks) {
+        this.name = name;
+        this.attacks = attacks;
+    }
+}
+
+const myPkmKey = "my-pkm";
+const yourPkmKey = "your-pkm";
+
+const pikachu = new Pokemon("pikachu", ["Eclair", "Vive-attaque", "Electacle", "Fatal-foudre"]);
+const avistar = new Pokemon("avistar", ["Pistolet à O", "Hurlement", "Hydrocanon", "Vive-attaque"]);
+
+function selectPikachu() {
+    selectPokemon(pikachu, avistar);
+}
+
+function selectAvistar() {
+    selectPokemon(avistar, pikachu);
+}
+
+function selectPokemon(myPkm, yourPkm) {
+    localStorage.setItem(myPkmKey, JSON.stringify(myPkm));
+    localStorage.setItem(yourPkmKey, JSON.stringify(yourPkm));
+    const pkmSelectScreen = document.getElementById("pkm-choice");
+    pkmSelectScreen.classList.add(hide);
+    loadMap(0);
+}
+
+function loadPokemon() {
+    return [
+        JSON.parse(localStorage.getItem(myPkmKey) || ""),
+        JSON.parse(localStorage.getItem(yourPkmKey) || "")
+    ];
+}
