b397f03bef8a659d7009386e27b05c7d62b8f8e2
Yann Zavattero <yann.zavattero@hotmail.com>
2023-01-19 13:15:51 +0100

    Remove implementation of exo

--- project/index.js
+++ project/index.js
@@ -3,14 +3,7 @@
 // Créer une fonction loadPokemonChoice devant : 
 // - Récupérer l'objet html contenant les 2 boutons de choix de pokémons, il possède l'id : pkm-choice
 // - Supprimer de ce dernier dans la liste des classes 'classList' à l'aide de la méthode 'remove' la classe 'hidden'.
-function loadPokemonChoice() {
-    const pokemonChoiceScreen = document.getElementById("pkm-choice");
-    pokemonChoiceScreen.classList.remove("hidden");
-}
 
 /* Fonctionnalité : 6 */
 // A faire :
 // Créer une fonction shutdown appellant la fonction de nettoyage unloadMap. 
-function shutdown() {
-    unloadMap();
-}
